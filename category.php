44d25ddb6f6bcade0ddd984d3fea403e1317d4d1
Eddy Martin <eddypmartin@gmail.com>
2022-04-01 16:40:45 -0400

    s9-c2 Template-part() gabarit/content-cours.php  menu_accueil dans front-page.php

--- category.php
+++ category.php
@@ -11,32 +11,14 @@
         <h2 class="formation__titre">Liste des cours du programme TIM</h2>
         <div class="formation__liste">
             <?php if (have_posts()):
-                while (have_posts()): the_post(); ?>
-                <?php
-                    // the_category();
-                    $categories =  get_the_category();
-                    //var_dump($categories);
-                   
+            while (have_posts()): the_post(); ?>
+                <?php 
+                if (is_category(array('cours','web','jeu','utilitaire','design','creation-3d','video'))):
+                get_template_part('gabarit/content','cours');    
+                else:
+                get_template_part('gabarit/content','macrame');    
+                endif
                 ?>
-            <article class="formation__cours  <?php  echo $categories[1]->slug;  ?> ">
-                <?php
-                $titre = get_the_title();
-                $titreFiltreCours = substr($titre, 7, -6);
-                $nbHeures = substr($titre, -6);
-                $sigleCours = substr($titre, 0, 7);
-                $descCours = get_the_excerpt();
-                ?>
-
-                <?php the_post_thumbnail("thumbnail"); ?>
-                <h3 class="cours__titre">
-                    <a href="<?php echo get_permalink(); ?>">
-                        <?= $titreFiltreCours; ?>
-                    </a>
-                </h3>
-                <div class="cours__nbre-heure"><?= $nbHeures; ?></div>
-                <p class="cours__sigle"><?= $sigleCours; ?> </p>
-                <p class="cours__desc"> <?= $descCours; ?></p>
-            </article>
             <?php endwhile ?>
             <?php endif ?>
         </div>
