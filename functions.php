0be9ec0ac4f8b4ea56793095b0c9d834d546795a
Eddy Martin <eddypmartin@gmail.com>
2022-04-05 12:43:06 -0400

    s10-c1 function.php pre_get_posts

--- functions.php
+++ functions.php
@@ -26,7 +26,7 @@ add_action( 'after_setup_theme', 'cidw_4w4_register_nav_menu', 0 );
 function prefix_nav_description( $item_output, $item) {
     if ( !empty( $item->description ) ) {
         $item_output = str_replace( '</a>',
-        '<hr><span class="menu-item-description">' . $item->description . '</span>' .  '</a>',
+        '<hr><span class="menu-item-description">' . $item->description . '</span><div class="menu-item-icone"></div></a>',
               $item_output );
     }
     return $item_output;
@@ -137,4 +137,65 @@ function my_register_sidebars() {
     /* Repeat register_sidebar() code for additional sidebars. */
 }
 
+function cidw_4w4_pre_get_posts(WP_Query $query)
+{
+
+    if (!is_admin() && is_main_query() && is_category(array('cours','web','jeu','design','utilitaire','creation-3d','video'))   )
+    {
+       //var_dump($query);
+       // die();
+       
+      $ordre = get_query_var('ordre');
+      // echo "-----ordre= "  . $ordre  . "-------------------------<br>";
+      $cle = get_query_var('cletri');
+      // echo "-----cle= "  . $cle  . "-------------------------<br>";
+
+       $query->set('order',  $ordre);
+       $query->set('orderby', $cle);
+       $query->set('postperpage','-1');
+
+
+    }
+    
+
+/*    
+  if (!is_admin() && is_main_query() && is_category(array('web','cours','design','video','utilitaire','creation-3d','jeu'))) 
+    {
+    //$ordre = get_query_var('ordre');
+    $query->set('posts_per_page', -1);
+    // $query->set('orderby', $cle);
+    $query->set('orderby', 'title');
+    // $query->set('order',  $ordre);
+    $query->set('order',  'ASC');
+    // var_dump($query);
+    // die();
+   }
+  */ 
+}
+
+
+function cidw_4w4_query_vars($params){
+// var_dump($params);
+// die();
+    $params[] = "ordre";
+    $params[] = "cletri";
+return $params;
+
+}
+/*
+    $params[] = "cletri";
+    $params[] = "ordre";
+    //$params["cletri"] = "title";
+    //var_dump($params); die();
+    return $params;
+}
+
+*/
+add_action('pre_get_posts', 'cidw_4w4_pre_get_posts');
+/* 
+Que le hook «pre_get_posts» se manisfeste juste avant que la requête WP_query soit exécuté. 
+Ce hook nous permettra d'adapter la requête avant d'exécuter cette requête 
+*/ 
+
+add_filter('query_vars', 'cidw_4w4_query_vars' );
 ?>
