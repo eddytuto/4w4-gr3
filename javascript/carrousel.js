6f1b3f3c7527d026ffbd78ee23443b6fa92e5a4b
Eddy Martin <eddypmartin@gmail.com>
2022-05-10 09:07:47 -0400

    s14-c2 carrousel animation test

--- javascript/carrousel.js
+++ javascript/carrousel.js
@@ -1,11 +1,13 @@
 (function(){
     
     console.log('vive le carrousel')
+    let galerie__img = document.querySelectorAll('.galerie img')
+    console.log( galerie__img.length)
     let boite__carrousel = document.querySelector('.boite__carrousel')
     let boite__carrousel__ferme = document.querySelector('.boite__carrousel__ferme')
     let boite__carrousel__navigation = document.querySelector('.boite__carrousel__navigation')
-    let galerie__img = document.querySelectorAll('.galerie img')
-    console.log( galerie__img.length)
+    let boite__carrousel__img = document.querySelector('.boite__carrousel__img')
+    
     /* Création d'un élément img */
      let elmImg  = document.createElement('img')
     /* Dans l'article de la boîte modale on ajoute la balise img */
@@ -13,14 +15,33 @@
     /* on parcour chacune des img de la galerie */
     let index = 0
     for (const img of galerie__img) {
-        let bouton = document.createElement('button')
+        /* Insertion de chacune des images de la galerie  dans la boite__carrousel */
+        let elmImg = document.createElement('img')
+        elmImg.setAttribute('src', img.getAttribute('src'))
+        console.log("elmImg src = " + elmImg.getAttribute('src'))
+        boite__carrousel__img.append(elmImg)
+
+        /* Creation des boutons radio */
+        let bouton = document.createElement('input')
+        bouton.setAttribute('type','radio')
+        bouton.setAttribute('class','bouton')
+        bouton.setAttribute('name','bouton')
+        bouton.setAttribute('checked','')
         bouton.dataset.index = index++
         boite__carrousel__navigation.append(bouton)
 
-        bouton.addEventListener('mousedown', function(){
-            elmImg.setAttribute('src', galerie__img[this.dataset.index].getAttribute('src'))
+        /* On écoute mousedown sur chacun des boutons */
+        bouton.addEventListener('change', function(e){
+            e.preventDefault
+            console.log(e)
+            console.log(this.getAttribute('checked'))
+            boite__carrousel__img.children[this.dataset.index].classList.remove('img--ouvrir')
+            boite__carrousel__img.children[this.dataset.index].classList.add('img--ouvrir')
+           
         })
 
+
+        /* Ouvrir la boite__carrousel */
         img.addEventListener('mousedown', function(){
             console.log(this.tagName)
             boite__carrousel.classList.add('boite__carrousel__ouvrir')
