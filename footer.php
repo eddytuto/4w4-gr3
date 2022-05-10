7d6008e339af9848bed71076154eb975e9ae9f61
Eddy Martin <eddypmartin@gmail.com>
2022-05-10 10:59:32 -0400

    s14-c2 teste animation carrousel

--- footer.php
+++ footer.php
@@ -46,6 +46,7 @@
 <div class="boite__carrousel">
     <button class="boite__carrousel__ferme">X</button>
     <section class="boite__carrousel__navigation"></section>
+    <section class="boite__carrousel__img"></section>
 </div>
 </body>
 <?php wp_footer(); ?>
--- javascript/carrousel.js
+++ javascript/carrousel.js
@@ -4,29 +4,44 @@
     let boite__carrousel = document.querySelector('.boite__carrousel')
     let boite__carrousel__ferme = document.querySelector('.boite__carrousel__ferme')
     let boite__carrousel__navigation = document.querySelector('.boite__carrousel__navigation')
+    let boite__carrousel__img = document.querySelector('.boite__carrousel__img')
     let galerie__img = document.querySelectorAll('.galerie img')
     console.log( galerie__img.length)
     /* Création d'un élément img */
-     let elmImg  = document.createElement('img')
-    /* Dans l'article de la boîte modale on ajoute la balise img */
-    boite__carrousel.append(elmImg)
+
     /* on parcour chacune des img de la galerie */
     let index = 0
     for (const img of galerie__img) {
-        let bouton = document.createElement('button')
-        bouton.dataset.index = index++
+
+        /* insertion des radio bouton dans boite__carrousel__navigation */
+        let bouton = document.createElement('input')
+        bouton.setAttribute('type','radio')
+        bouton.setAttribute('checked','')
+        bouton.setAttribute('name','bouton')
+        bouton.setAttribute('class','bouton')
+        bouton.dataset.index = index
         boite__carrousel__navigation.append(bouton)
 
+        /* insertion des elmImg de la galerie__img dans boite__carrousel__img */
+        let elmImg  = document.createElement('img')
+
+        //console.log(img.src)
+        elmImg.img.setAttribute('src', img.src)
+        /* Dans l'article de la boîte modale on ajoute la balise img */
+        boite__carrousel__img.append(elmImg)
+
+
         bouton.addEventListener('mousedown', function(){
-            elmImg.setAttribute('src', galerie__img[this.dataset.index].getAttribute('src'))
+           // elmImg.setAttribute('src', galerie__img[this.dataset.index].getAttribute('src'))
         })
 
         img.addEventListener('mousedown', function(){
-            console.log(this.tagName)
-            boite__carrousel.classList.add('boite__carrousel__ouvrir')
-            console.log(this.getAttribute('src'))
-            elmImg.setAttribute('src', this.getAttribute('src'))
+           // console.log(this.tagName)
+          //  boite__carrousel.classList.add('boite__carrousel__ouvrir')
+           // console.log(this.getAttribute('src'))
+           // elmImg.setAttribute('src', this.getAttribute('src'))
         })
+       index++ 
     }
 
     boite__carrousel__ferme.addEventListener('mousedown', function(){
